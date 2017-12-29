1747417f5d578120580cd49591b397bea8c0ccaf
Muhammad Inam Ur Rasheed <mohdinamshah@gmail.com>
2017-12-30 00:56:18 +0530

    Added required config.

---
+++ docroot/sites/default/default.settings.local.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'alimdaad',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
--- docroot/sites/default/settings.env.php
+++ docroot/sites/default/settings.env.php
@@ -1,8 +1,16 @@
 <?php
 
+$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/default';
+$settings['file_private_path']  = DRUPAL_ROOT . '/sites/default/files/private';
+$settings['install_profile'] = 'shade';
+
+// Overridden below.
+$settings['hash_salt'] = 'x71-h0ba8aiSubbiep1Quitach6Keiiekah2MuPaowai6i_I5ttE4ChqTR9eT3WmC4m-fP98mg';
+
+/**
+ * Environment specific overrides. like dev, prod
+ * Keep this at bottom of file to override variables.
+ */
 if (file_exists('../../conf/php/settings.php')) {
   include '../../conf/php/settings.php';
 }
-
-$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/default';
-$settings['file_private_path']  = DRUPAL_ROOT . '/sites/default/files/private';
